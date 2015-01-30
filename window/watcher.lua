27e0a1a350d8b77ac230e863f19b8d8b380b4354
Marshall Brekka <marshallsmedia@gmail.com>
2015-01-29 16:51:24 -0800

    some progress on cleaning up the interface

--- window/watcher.lua
+++ window/watcher.lua
@@ -1,83 +1,103 @@
+-- copied from https://gist.github.com/tmandry/a5b1ab6d6ea012c1e8c5
+
 local events = require "hs.uielement.watcher"
 local application = require "hs.application"
-local watchers = {}
 
--- copied from https://gist.github.com/tmandry/a5b1ab6d6ea012c1e8c5
--- intend to only take some parts
--- function init()
---   appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
---   appsWatcher:start()
+local watchers = {}
+local windowEventWatcher = nil
+local windowEvents = {"open"   = events.windowCreated,
+                      "close"  = events.elementDestroyed,
+                      "move"   = events.windowMoved,
+                      "resize" = events.windowResized}
+
+local function start(callback)
+  if not windowEventWatcher then error("watcher already running, can't start")
+  windowEventWatcher = callback
+  appsWatcher = application.watcher.new(handleGlobalAppEvent)
+  appsWatcher:start()
  
---   -- Watch any apps that already exist
---   local apps = hs.application.runningApplications()
---   for i = 1, #apps do
---     if apps[i]:title() ~= "Hammerspoon" then
---       watchApp(apps[i], true)
---     end
---   end
--- end
+  -- Watch any apps that already exist
+  local apps = hs.application.runningApplications()
+  for i = 1, #apps do
+    if apps[i]:title() ~= "Hammerspoon" then
+      watchApp(apps[i], true)
+    end
+  end
+end
+
+local function stop()
+   for pid, watchers in ipairs(watchers) do
+      cleanupAppWatchers(pid)
+   end
+   windowEventWatcher = nil
+end
+
+local function cleanupWindowWatcher(pid, windowId)
+   watchers[pid].windows[windowId]:stop()
+   watchers[pid].windows[windowId] = nil
+end
+
+local function cleanupAppWatchers(pid)
+   for id, watcher in ipairs(watchers[pid]) do
+      watcher:stop()
+   end
+   watchers[pid] = nil
+end
+
+
+local function handleGlobalAppEvent(name, event, app)
+  if event == hs.application.watcher.launched then
+    watchApp(app)
+  elseif event == hs.application.watcher.terminated then
+    -- Clean up
+    cleanupAppWatchers(app:pid())
+  end
+end
+
+local function watchApp(app, initializing)
+  if watchers[app:pid()] then return end
  
--- function handleGlobalAppEvent(name, event, app)
---   if     event == hs.application.watcher.launched then
---     watchApp(app)
---   elseif event == hs.application.watcher.terminated then
---     -- Clean up
---     local appWatcher = watchers[app:pid()]
---     if appWatcher then
---       appWatcher.watcher:stop()
---       for id, watcher in pairs(appWatcher.windows) do
---         watcher:stop()
---       end
---       watchers[app:pid()] = nil
---     end
---   end
--- end
+  local watcher = app:newWatcher(handleAppEvent)
+  watchers[app:pid()] = {watcher = watcher, windows = {}}
  
--- function watchApp(app, initializing)
---   if watchers[app:pid()] then return end
+  watcher:start({events.windowCreated, events.focusedWindowChanged})
  
---   local watcher = app:newWatcher(handleAppEvent)
---   watchers[app:pid()] = {watcher = watcher, windows = {}}
+  -- Watch any windows that already exist
+  for i, window in pairs(app:allWindows()) do
+    watchWindow(window, initializing)
+  end
+end
  
---   watcher:start({events.windowCreated, events.focusedWindowChanged})
+local function handleAppEvent(element, event)
+  if event == events.windowCreated then
+    watchWindow(element)
+  elseif event == events.focusedWindowChanged then
+    -- Handle window change
+  end
+end
  
---   -- Watch any windows that already exist
---   for i, window in pairs(app:allWindows()) do
---     watchWindow(window, initializing)
---   end
--- end
+local function watchWindow(win, initializing)
+  local appWindows = watchers[win:application():pid()].windows
+  if win:isStandard() and not appWindows[win:id()] then
+    local watcher = win:newWatcher(handleWindowEvent, {pid=win:pid(), id=win:id()})
+    appWindows[win:id()] = watcher
  
--- function handleAppEvent(element, event)
---   if event == events.windowCreated then
---     watchWindow(element)
---   elseif event == events.focusedWindowChanged then
---     -- Handle window change
---   end
--- end
+    watcher:start({events.elementDestroyed, events.windowResized, events.windowMoved})
  
--- function watchWindow(win, initializing)
---   local appWindows = watchers[win:application():pid()].windows
---   if win:isStandard() and not appWindows[win:id()] then
---     local watcher = win:newWatcher(handleWindowEvent, {pid=win:pid(), id=win:id()})
---     appWindows[win:id()] = watcher
+    if not initializing then
+      hs.alert.show('window created: '..win:id()..' with title: '..win:title())
+    end
+  end
+end
  
---     watcher:start({events.elementDestroyed, events.windowResized, events.windowMoved})
- 
---     if not initializing then
---       hs.alert.show('window created: '..win:id()..' with title: '..win:title())
---     end
---   end
--- end
- 
--- function handleWindowEvent(win, event, watcher, info)
---   if event == events.elementDestroyed then
---     watcher:stop()
---     watchers[info.pid].windows[info.id] = nil
---   else
---     -- Handle other events...
---   end
---   hs.alert.show('window event '..event..' on '..info.id)
--- end
+local function handleWindowEvent(win, event, watcher, info)
+  if event == events.elementDestroyed then
+    
+  else
+    -- Handle other events...
+  end
+  hs.alert.show('window event '..event..' on '..info.id)
+end
  
 -- init()
 
