e491c252628a0cb0d913996c9f0cd940b1c15229
Marshall Brekka <marshallsmedia@gmail.com>
2015-02-04 18:51:20 -0800

    fns for moving windows between screens and moving cursor between screens

--- screen/screen.lua
+++ screen/screen.lua
@@ -1,6 +1,6 @@
-window = require "awesomeosx.window.window"
-screen = require "hs.screen"
-mouse  = require "hs.mouse"
+local window = require "awesomeosx.window.window"
+local screen = require "hs.screen"
+local mouse  = require "hs.mouse"
 
 local function focusWindowUnderMouse ()
    window.focusIfNotFocused(window.windowUnderMouse())
@@ -9,7 +9,7 @@ end
 local function focus (screenNumber)
    local screens = screen.allScreens()
    local frame = screens[screenNumber]:frame()
-   mouse.set({x = frame.x, y = frame.y})
+   mouse.set({x = frame.x + 10, y = frame.y + 10})
    focusWindowUnderMouse()
 end
 
--- window/window.lua
+++ window/window.lua
@@ -1,6 +1,7 @@
 local window = require "hs.window"
 local mouse  = require "hs.mouse"
 local events = require "hs.eventtap"
+local screen = require "hs.screen"
 local etypes = require "hs.eventtap.event"
 
 local windowUnderPoint = function(point)
@@ -50,10 +51,34 @@ local stopWatchMouse = function(watcher)
       watcher:stop()
 end
 
+local _moveToScreen = function(window, screen)
+   local frame = screen:frame()
+   window:setTopLeft({x = frame.x, y = frame.y})
+end
+
+local moveToScreen = function(window, screenNumber)
+   local screen = screen.allScreens()[screenNumber]:frame()
+   _moveToScreen(window, screen)
+end
+
+local moveToNextScreen = function(window)
+   local screen = window:screen():next()
+   _moveToScreen(window, screen)
+end
+
+local moveToPreviousScreen = function(window)
+   local screen = window:screen():previous()
+   _moveToScreen(window, screen)
+end
+
 return {
-       windowUnderPoint = windowUnderPoint,
-       windowUnderMouse = windowUnderMouse,
+       windowUnderPoint  = windowUnderPoint,
+       windowUnderMouse  = windowUnderMouse,
        focusIfNotFocused = focusIfNotFocused,
-       watchMouse = watchMouse,
-       stopWatchMouse = stopWatchMouse
+       watchMouse        = watchMouse,
+       stopWatchMouse    = stopWatchMouse,
+       moveToScreen      = moveToScreen,
+       moveToNextScreen  = moveToNextScreen,
+       moveToPreviousScreen  = moveToPreviousScreen,
+       focusedWindow     = window.focusedWindow
        }
